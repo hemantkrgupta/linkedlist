e42e5533c50db0b2ce8068b842e05b0745c5a129
Hemant Kumar <hemant.gt.kumar@gmail.com>
2021-06-25 09:56:33 +0530

    Added nodes

--- src/com/linklistimpl/SinglePointerNode.java
+++ src/com/linklistimpl/SinglePointerNode.java
@@ -1,5 +1,44 @@
 package com.linklistimpl;
 
+/**
+ * This class represents a node to be used to construct a singly linked list. It contains data
+ * and next node pointer.
+ * @author Hemant
+ *
+ */
 public class SinglePointerNode {
-//
+  private int data;
+  private SinglePointerNode next;
+  
+  /**
+   * Constructs a node with provided int data and next node as null.
+   * @param data Node data
+   */
+  public SinglePointerNode(int data) {
+    this.data = data;
+  }
+  
+  /**
+   * This function returns the data in the node.
+   * @return Integer data
+   */
+  public int getData() {
+    return this.data;
+  }
+  
+  /**
+   * This function sets the next node pointer to provided node.
+   * @param node Next node
+   */
+  public void setNextNode(SinglePointerNode node) {
+    this.next = node;
+  }
+  
+  /**
+   * This function returns the next node pointed by this node.
+   * @return Next node
+   */
+  public SinglePointerNode getNextNode() {
+    return this.next;
+  }
 }
---
+++ src/com/linklistimpl/TwoPointerNode.java
@@ -0,0 +1,61 @@
+package com.linklistimpl;
+
+/**
+ * This class represents a node to be used to construct doubly linked list. It contains
+ * data, previous node pointer and next node pointer.
+ * @author Hemant
+ *
+ */
+public class TwoPointerNode {
+  private int data;
+  private TwoPointerNode prev;
+  private TwoPointerNode next;
+  
+  /**
+   * Constructs a node with provided data.
+   * @param data Int data to store in constructed node.
+   */
+  public TwoPointerNode(int data) {
+    this.data = data;
+  }
+  
+  /**
+   * This function returns the data stored in node.
+   * @return Data
+   */
+  public int getData() {
+    return this.data;
+  }
+  
+  /**
+   * This functions sets the next pointer to provided node.
+   * @param nextNode Next node
+   */
+  public void setNext(TwoPointerNode nextNode) {
+    this.next = nextNode;
+  }
+  
+  /**
+   * This function returns the next node pointed by this node.
+   * @return Next node
+   */
+  public TwoPointerNode getNext() {
+    return this.next;
+  }
+  
+  /**
+   * This function sets the previous pointer to provided node.
+   * @param prevNode Previous node
+   */
+  public void setPrev(TwoPointerNode prevNode) {
+    this.prev = prevNode;
+  }
+  
+  /**
+   * This function return the previous node pointed by this node.
+   * @return Previous node
+   */
+  public TwoPointerNode getPrev() {
+    return this.prev;
+  }
+}
